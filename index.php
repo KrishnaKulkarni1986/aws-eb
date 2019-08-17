f460940b91ab7a7ae72453031c6cc27fc80575cf
KrishnaKulkarni1986 <54196404+KrishnaKulkarni1986@users.noreply.github.com>
2019-08-17 13:06:25 +0530

    Update index.php

--- index.php
+++ index.php
@@ -3,7 +3,8 @@
 <body>
 
 <?php
-echo "Elastic beanstalk with codepipeline withe developer tools with migration"
+echo "
+AWS Elastic Beanstalk is a cloud deployment and provisioning service that automates the process of getting applications set up on the Amazon Web Services (AWS) infrastructure. To use the service, developers just have to upload their applications."
  ?>
 
 </body>
